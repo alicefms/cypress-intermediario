b3d2c41e79ef0e30b519803639f25bbd31e021d3
Alice Souza <alicefms@hotmail.com>
2023-08-22 12:34:29 -0300

    refatoração do teste de login com sucesso

--- cypress/e2e/gui/login.cy.js
+++ cypress/e2e/gui/login.cy.js
@@ -1,10 +1,7 @@
 
 describe('Login', () => {
   it('login com sucesso', () => {
-    cy.visit('http://localhost')
-    cy.get('#user_login').type('root')
-    cy.get('#user_password').type('Senh4fake')
-    cy.get('#new_user > .submit-container > .btn').click()
+    cy.login()
     cy.get('.header-user-avatar').should('be.visible')
 
   })
